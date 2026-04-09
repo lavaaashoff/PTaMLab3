f41d007c464de3002ec8e57cf29868dcf7867842
lavaaash <vital229008@Mail.ru>
2026-04-09 18:56:35 +0700

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MenuDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new System.Windows.Forms.MenuStrip();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Location = new System.Drawing.Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new System.Drawing.Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainForm";
+            Text = "MainForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+using MenuLibrary;
+
+namespace MenuDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent(); // вызов из Designer.cs
+            LoadMenu();
+        }
+
+        // Обработчики пунктов меню
+        public void Others() => ShowHandler(nameof(Others));
+        public void Stuff() => ShowHandler(nameof(Stuff));
+        public void Orders() => ShowHandler(nameof(Orders));
+        public void Docs() => ShowHandler(nameof(Docs));
+        public void Departs() => ShowHandler(nameof(Departs));
+        public void Towns() => ShowHandler(nameof(Towns));
+        public void Posts() => ShowHandler(nameof(Posts));
+        public void Window() => ShowHandler(nameof(Window));
+        public void Content() => ShowHandler(nameof(Content));
+        public void About() => ShowHandler(nameof(About));
+
+        private void LoadMenu()
+        {
+            try
+            {
+                var builder = new MenuBuilder("menu.txt", this);
+                builder.BuildMenu(menuStrip1); // имя из Designer
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    $"Ошибка загрузки меню:\n{ex.Message}",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private static void ShowHandler(string handlerName)
+        {
+            MessageBox.Show(
+                $"Вызван обработчик: {handlerName}",
+                "Обработчик пункта меню",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
